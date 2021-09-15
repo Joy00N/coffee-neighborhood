13047c11edf2f3dc6e63fbfaef488602dec76ea7
Yoonhee <yoonhee.jo.cs@gmail.com>
2021-09-15 13:20:32 -0400

    initial commit

---
+++ src/main/java/com/joy/coffeeneighborhood/CoffeeNeighborhoodApplication.java
@@ -0,0 +1,13 @@
+package com.joy.coffeeneighborhood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeNeighborhoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffeeNeighborhoodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joy/coffeeneighborhood/CoffeeNeighborhoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joy.coffeeneighborhood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeNeighborhoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
